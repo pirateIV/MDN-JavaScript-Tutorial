aaf68732b9d21d54bb7da031c314e8e84dc983fe
ben64103 <benabolade@gmail.com>
2023-08-02 20:16:37 +0100

    learning progress 2

--- Active learning - Top 5 searches/script.js
+++ Active learning - Top 5 searches/script.js
@@ -1,21 +1,21 @@
 const searchInput = document.getElementById("searchInput");
 const searchBtn = document.getElementById("btn");
 
-const list = document.getElementById("list")
+const list = document.getElementById("list");
 
 let max_items = 5;
-let searchHistoryArr = []
+let searchHistoryArr = [];
 searchBtn.addEventListener("click", (e) => {
-  let searchVal = searchInput.value
+  let searchVal = searchInput.value;git 
 
-  list.innerHTML =  ''
+  list.innerHTML = "";
 
-  searchHistoryArr.push(searchVal)
+  searchHistoryArr.push(searchVal);
   searchHistoryArr.forEach((historySearchVal) => {
-    const listItem = document.createElement("li")
-    const text = document.createTextNode(historySearchVal)
+    const listItem = document.createElement("li");
+    const text = document.createTextNode(historySearchVal);
 
-    listItem.textContent = text
-    list.appendChild(listItem)
-  } )
+    listItem.textContent = text;
+    list.appendChild(listItem);
+  });
 });
