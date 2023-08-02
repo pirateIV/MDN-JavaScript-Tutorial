2ad0f9efb9236a1a21ee0f4d1876e6b4dd427fa5
ben64103 <benabolade@gmail.com>
2023-08-02 23:00:52 +0100

    ...

---
+++ Test/Arrays - 2/script.js
@@ -0,0 +1,21 @@
+// Add your code here
+
+let myString = 'Ryu+Ken+Chun-Li+Cammy+Guile+Sakura+Sagat+Juri';
+
+let myArray = myString.split("+")
+
+// Don't edit the code below here!
+
+section.innerHTML = ' ';
+let para1 = document.createElement('p');
+para1.textContent = `Array: ${ myArray }`;
+
+let para2 = document.createElement('p');
+para2.textContent = `The length of the array is ${ arrayLength }.`;
+
+let para3 = document.createElement('p');
+para3.textContent = `The last item in the array is "${ lastItem }".`;
+
+section.appendChild(para1);
+section.appendChild(para2);
+section.appendChild(para3);
