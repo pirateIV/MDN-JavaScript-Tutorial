92c1e79e34e9c75b43a2a392efb4c73848ea9533
ben64103 <benabolade@gmail.com>
2023-08-02 22:43:22 +0100

    section arrays

---
+++ Active Learning/Active learning - Top 5 searches/script.js
@@ -0,0 +1,24 @@
+const searchInput = document.getElementById("searchInput");
+const searchBtn = document.getElementById("btn");
+
+const list = document.getElementById("list");
+
+let max_items = 5;
+let searchHistoryArr = [];
+searchBtn.addEventListener("click", (e) => {
+  let searchVal = searchInput.value;
+
+  list.innerHTML = "";
+
+  searchHistoryArr.unshift(searchVal);
+  for(let i = 0; i < searchHistoryArr.length; i++) {
+    list.innerHTML += `<li>${searchHistoryArr[i]}</li>`
+  }
+
+  if(searchHistoryArr.length >= max_items) {
+    searchHistoryArr.pop()
+  }
+  searchInput.value = ''
+  searchInput.focus()
+  console.log(searchHistoryArr)
+});
