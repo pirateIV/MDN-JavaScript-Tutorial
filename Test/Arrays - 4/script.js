3dd43ff2880f4a6947411d8dd20964a164766d36
ben64103 <benabolade@gmail.com>
2023-08-05 13:57:54 +0100

    created branch main

---
+++ Test/Arrays - 4/script.js
@@ -0,0 +1,24 @@
+const birds = [ "Parrots", "Falcons", "Eagles", "Emus", "Caracaras", "Egrets" ];
+
+// Add your code here
+
+// for(let i = 0; i <= birds.length; i++) {
+//   birds.splice(birds[i])
+// }
+
+birds.forEach((bird, index) => {
+    
+})
+
+
+// Don't edit the code below here!
+
+section.innerHTML = ' ';
+
+section.innerHTML = birds
+
+const para1 = document.createElement('p');
+para1.textContent = eBirds;
+
+section.appendChild(para1);
+    
